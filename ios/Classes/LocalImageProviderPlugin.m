27ec97ed20cf3d786dcc6be2ae1f94a798ab227f
liqun <lq@fir.im>
2019-10-21 18:09:38 +0800

    local_image_provider-Swift 去掉尖括号

--- ios/Classes/LocalImageProviderPlugin.m
+++ ios/Classes/LocalImageProviderPlugin.m
@@ -1,5 +1,5 @@
 #import "LocalImageProviderPlugin.h"
-#import <local_image_provider/local_image_provider-Swift.h>
+#import "local_image_provider-Swift.h"
 
 @implementation LocalImageProviderPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
